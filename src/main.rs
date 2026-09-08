846ad0668b1763ca44bab266696aa2c95d260cb0
MrMozzarella-d <timhartmeier@gmail.com>
2026-09-08 18:56:21 +0200

    feat: implement basic logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use clap;
+use clap::{ Parser, arg, command, ArgGroup };
+
+#[derive(Parser, Debug)]
+#[command(name = "strmul", about = "CLI-tool to multiply strings")]
+struct Args {
+    string: String,
+    count: usize,
+
+    #[arg(long)]
+    head: bool,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args=  Args::parse();
+    if args.head {
+        for _ in 0..args.count { println!("{}", args.string) }
+    } else {
+        for _ in 0..args.count { print!("{}", args.string) }
+    }
 }
